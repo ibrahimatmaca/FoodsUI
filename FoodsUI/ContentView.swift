84690d56cc5393ad6f978a652772de6326719839
ibrahimatmaca <ibrahim.atmaca@bil.omu.edu.tr>
2022-08-18 01:05:08 +0300

    Initial Commit

---
+++ FoodsUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodsUI
+//
+//  Created by İbrahim Atmaca on 18.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodsUI/FoodsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodsUIApp.swift
+//  FoodsUI
+//
+//  Created by İbrahim Atmaca on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
